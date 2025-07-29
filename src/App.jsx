83288295d97772313a6a7bfdd8fb9fac2a33fc53
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-29 23:25:22 +0530

    imported components and added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
-import Header from './components/header/Header'
+import "./App.css";
+import Header from "./components/header/Header";
+import PopularMovies from "./components/movies/PopularMovies";
 
 function App() {
   return (
-    <div className='main-container'>
+    <div className="main-container">
       <Header></Header>
-      <h1>Hello world</h1>
+      <PopularMovies></PopularMovies>
+      {/* <h1>Hello world</h1> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
