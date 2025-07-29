e2da5f62300ad30b8d551e1ca811efb419071737
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 00:02:32 +0530

    added/changed more styles

--- src/components/movies/PopularMovies.jsx
+++ src/components/movies/PopularMovies.jsx
@@ -8,18 +8,18 @@ const PopularMovies = () => {
   useEffect(() => {
     try {
       const fetchMovies = async () => {
-        const url =
-          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-        const options = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTMzNTc5NTAzZDgyN2NlMjJjNjI3NTgwMTU5ZTZlNiIsIm5iZiI6MTcyNzc1NzQwMS44MjIsInN1YiI6IjY2ZmI3YzU5ZDgwNjQxNjViZGYxNmFjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQSwgX4n_teHX61idIJa9N0v5BLYh8tdL374pRU9HxY",
-          },
-        };
+        // const url =
+        //   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+        // const options = {
+        //   method: "GET",
+        //   headers: {
+        //     accept: "application/json",
+        //     Authorization:
+        //       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTMzNTc5NTAzZDgyN2NlMjJjNjI3NTgwMTU5ZTZlNiIsIm5iZiI6MTcyNzc1NzQwMS44MjIsInN1YiI6IjY2ZmI3YzU5ZDgwNjQxNjViZGYxNmFjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bQSwgX4n_teHX61idIJa9N0v5BLYh8tdL374pRU9HxY",
+        //   },
+        // };
 
-        const res = await fetch(url, options);
+        const res = await fetch("https://first-backend-eight.vercel.app/popular_movies");
         const data = await res.json();
         console.log(data.results[0]);
 
@@ -33,7 +33,7 @@ const PopularMovies = () => {
   // console.log(movies[0]);
   return (
     <div className="movie-container">
-      <h1>PopularMovies</h1>
+      <h1>Popular Movies</h1>
       <div className="flex-container">
         {movies ? (
           movies.map((movie) => <Card {...movie}></Card>)
