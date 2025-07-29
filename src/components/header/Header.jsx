12e79581968b7b5d6bb1ae59765b0781e73ee138
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 00:04:54 +0530

    added my gitHub link

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,18 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-container">
+      <div className="git-logo">
+        <i>GitHub: </i>
+        <a href="https://github.com/SanjeevKumar3662" target="_blank">
+          SanjeevKumar3662
+        </a>
+      </div>
+
       <div className="left-header">
-        <span className="logo">SanjeevKumar</span>
         <a href="">Movies</a>
         <a href="">TV Shows</a>
       </div>
-      <h2>Header</h2>
+      {/* <h2>Header</h2> */}
       <div className="right-header">
         <a href="">link 1</a>
 
