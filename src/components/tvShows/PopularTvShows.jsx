2a5ef9427be5d6dc52252eb747cbc33dea0f6968
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 22:10:24 +0530

    added popular TvShows component

---
+++ src/components/tvShows/PopularTvShows.jsx
@@ -0,0 +1,38 @@
+
+import "./PopularTvShows.css";
+import { useEffect, useState } from "react";
+import Card from "../card/Card";
+
+const PopularTvShows = () => {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    try {
+      const fetchMovies = async () => {
+
+        const res = await fetch("https://first-backend-eight.vercel.app/popular_tv");
+        const data = await res.json();
+        console.log(data.results[0]);
+
+        setMovies(data.results);
+      };
+      fetchMovies();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  // console.log(movies[0]);
+  return (
+    <div className="movie-container">
+      <h1>Popular Movies</h1>
+      <div className="flex-container">
+        {movies ? (
+          movies.map((movie) => <Card key={movie.id} {...movie}></Card>)
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    </div>
+  );
+};
+export default PopularTvShows;
