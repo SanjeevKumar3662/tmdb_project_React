0fedab2855f04391801302428c49c89dfd2d665e
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 23:59:25 +0530

    added link components for routing and added some media queries for mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,20 +5,24 @@ export default function Header() {
   return (
     <div className="header-container">
       <div className="git-logo">
-        <button className="github-btn">
+        <button className="btn github-btn">
           <a href="https://github.com/SanjeevKumar3662" target="_blank">
             <div className="git-logo">
               <img src="./GitHub-logo.png" alt="" />
             </div>
-            SanjeevKumar3662
+            <span >SanjeevKumar3662</span>
           </a>
         </button>
       </div>
 
       <div className="left-header">
         {/* <a href="">Movies</a> */}
-        <Link to={"/"} >Movies</Link>
-        <Link to={"/popular_tv_shows"} >TV Shows</Link>
+        <Link to={"/"}>
+          <button className="btn">Movies</button>
+        </Link>
+        <Link to={"/popular_tv_shows"}>
+          <button className="btn">TV Shows</button>
+        </Link>
         {/* <a href="">TV Shows</a> */}
       </div>
       {/* <h2>Header</h2> */}
