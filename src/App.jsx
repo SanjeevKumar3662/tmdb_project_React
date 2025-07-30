fbe9dac4219e93327bd5c4d34b0b7b7eb1085955
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 23:29:10 +0530

    created routes to pop movies and tvs pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
-import { HashRouter, Router, Route } from "react-router-dom";
+import { HashRouter, Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
-import PopularMovies from "./components/movies/PopularMovies";
-import PopularTvShows from "./components/tvShows/PopularTvShows";
+import PopularMovies from "./pages/movies/PopularMovies";
+import PopularTvShows from "./pages/tvShows/PopularTvShows";
 
 function App() {
   return (
-    <div className="main-container">
-      <Header></Header>
-      {/* <PopularMovies></PopularMovies> */}
-      <PopularTvShows></PopularTvShows>
-      {/* <h1>Hello world</h1> */}
-    </div>
+    <HashRouter>
+      <div className="main-container">
+        <Header></Header>
+        {/* <PopularMovies></PopularMovies> */}
+        {/* <PopularTvShows></PopularTvShows> */}
+        <Routes>
+          <Route path="/" element={<PopularMovies />} />
+          <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
