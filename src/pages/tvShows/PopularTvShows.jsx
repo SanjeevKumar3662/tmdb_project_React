46f1d1e6e98442094807e336f7e6ab15f5f7b48e
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 23:27:55 +0530

    created a separate pages folder add movies and tvs page

---
+++ src/pages/tvShows/PopularTvShows.jsx
@@ -0,0 +1,38 @@
+import "./PopularTvShows.css";
+import { useEffect, useState } from "react";
+import Card from "../../components/card/Card";
+
+const PopularTvShows = () => {
+  const [TvShows, setPopTvShows] = useState(null);
+
+  useEffect(() => {
+    try {
+      const fetchTvShows = async () => {
+        const res = await fetch(
+          "https://first-backend-eight.vercel.app/popular_tv"
+        );
+        const data = await res.json();
+        console.log(data.results[0]);
+
+        setPopTvShows(data.results);
+      };
+      fetchTvShows();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  // console.log(TvShows[0]);
+  return (
+    <div className="movie-container">
+      <h1>Popular TvShows</h1>
+      <div className="flex-container">
+        {TvShows ? (
+          TvShows.map((tvShow) => <Card key={tvShow.id} {...tvShow}></Card>)
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    </div>
+  );
+};
+export default PopularTvShows;
