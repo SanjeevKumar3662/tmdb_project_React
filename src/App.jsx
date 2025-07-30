120f0a311035108572f7e6f01d6023104b28e0e1
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-07-30 22:37:56 +0530

    installed react-router and imported it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import { HashRouter, Router, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import PopularMovies from "./components/movies/PopularMovies";
+import PopularTvShows from "./components/tvShows/PopularTvShows";
 
 function App() {
   return (
     <div className="main-container">
       <Header></Header>
-      <PopularMovies></PopularMovies>
+      {/* <PopularMovies></PopularMovies> */}
+      <PopularTvShows></PopularTvShows>
       {/* <h1>Hello world</h1> */}
     </div>
   );
