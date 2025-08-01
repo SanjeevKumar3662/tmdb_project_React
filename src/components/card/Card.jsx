ce41be064eb06b29e9c29a72b425a1ce0f2ea104
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-08-01 22:59:45 +0530

    added link to movie and tv details pages

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,18 @@
 import "./Card.css";
+import { Link } from "react-router-dom";
 
-const Card = ({ title, name, poster_path, release_date, first_air_date }) => {
+const Card = ({ title, name, poster_path, release_date, first_air_date, id}) => {
+  
   return (
     <div className="card">
-      <img
-        className="poster"
-        src={`https://image.tmdb.org/t/p/original${poster_path}`}
-        alt=""
-      />
+      {/* only movies have release date, so is this is true that means it movie else a tv show */}
+      <Link to={`/${release_date?"movie_details":"tv_shows"}/${id}`}>
+        <img
+          className="poster"
+          src={`https://image.tmdb.org/t/p/original${poster_path}`}
+          alt=""
+        />
+      </Link>
       <div className="title">{title || name}</div>
       <div className="release-date">{release_date || first_air_date}</div>
     </div>
