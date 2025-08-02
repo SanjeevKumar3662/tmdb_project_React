498c4353ae58759d75ea80e66cb7abbde936b5e9
SanjeevKumar3662 <sanjeevkumar3662work@gmail.com>
2025-08-02 10:52:58 +0530

    add links and routes to tv details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Header from "./components/header/Header";
 import PopularMovies from "./pages/movies/PopularMovies";
 import PopularTvShows from "./pages/tvShows/PopularTvShows";
 import MovieDetails from "./pages/movies/MovieDetails";
+import TvShowDetails from "./pages/tvShows/TvShowDetails";
 
 function App() {
   return (
     <HashRouter>
       <div className="main-container">
         <Header></Header>
-        {/* <PopularMovies></PopularMovies> */}
-        {/* <PopularTvShows></PopularTvShows> */}
+  
         <Routes>
           <Route path="/" element={<PopularMovies />} />
           <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
           <Route path="/movie_details/:id" element={<MovieDetails/>}/>
+          <Route path="/tv_shows_details/:id" element={<TvShowDetails/>}/>
         </Routes>
       </div>
     </HashRouter>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ title, name, poster_path, release_date, first_air_date, id}) => 
   return (
     <div className="card">
       {/* only movies have release date, so is this is true that means it movie else a tv show */}
-      <Link to={`/${release_date?"movie_details":"tv_shows"}/${id}`}>
+      <Link to={`/${release_date?"movie_details":"tv_shows_details"}/${id}`}>
         <img
           className="poster"
           src={`https://image.tmdb.org/t/p/original${poster_path}`}
