e8ac0ddcc6fec27ec2ec75a444b38a27cfbd6c41
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-04 13:50:05 +0530

    made page no. editable

--- src/components/pageNav/PageNav.jsx
+++ src/components/pageNav/PageNav.jsx
@@ -1,12 +1,23 @@
-import "./PageNav.css"
+import { useState } from "react";
+import "./PageNav.css";
 
-const PageNav = ({prevClick,nextClick, page}) => {
+const PageNav = ({ prevClick, nextClick, page, setPage }) => {
+  const [isEdit, setEdit] = useState(false);
+  function onPageEditClick(e) {
+    setPage(e.target.value)
+    setEdit(() => !isEdit);
+  }
+
   return (
     <section className="page-nav">
-      <button onClick={prevClick}>
-        Prev
-      </button>
-      <span>On Page: {page}</span>
+      <button onClick={prevClick}>Prev</button>
+
+      {isEdit ? (
+        <input onBlur={onPageEditClick} type="text" placeholder="set page no." ></input>
+      ) : (
+        <button onClick={()=>setEdit(() => !isEdit)}>On Page: {page}</button>
+      )}
+
       <button onClick={nextClick}>Next</button>
     </section>
   );
