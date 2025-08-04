16742d7dabe628cc25f498f92968fcc8941f7884
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-04 12:29:59 +0530

    updated links to new page route

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,12 +2,12 @@ import noImage from "/src/assets/noImage.png"
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-const Card = ({ title, name, poster_path, release_date, first_air_date, id}) => {
+const Card = ({ title, name, poster_path, release_date, first_air_date, id,page}) => {
   
   return (
     <div className="card">
       {/* only movies have release date, so is this is true that means it movie else a tv show */}
-      <Link target="blank" to={`/${release_date?"movie_details":"tv_shows_details"}/${id}`}>
+      <Link target="blank" to={`/${release_date?"movie_details":"tv_shows_details"}/${page}/${id}`}>
       {/* remember _blank will open a new window every time */}
         <img
           className="poster"
