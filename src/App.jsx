3d6611b3cc8eec052d2ff03c2d1ac2e984de18c2
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-04 15:26:19 +0530

    moved browserRouter to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useLocation, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import PopularMovies from "./pages/movies/PopularMovies";
 import PopularTvShows from "./pages/tvShows/PopularTvShows";
@@ -7,19 +7,23 @@ import MovieDetails from "./pages/movies/MovieDetails";
 import TvShowDetails from "./pages/tvShows/TvShowDetails";
 
 function App() {
+  const location = useLocation();
+  const isDetails =
+    location.pathname.startsWith("/movie_details") ||
+    location.pathname.startsWith("/tv_shows_details");
+
   return (
-    <BrowserRouter>
-      <div className="main-container">
-        <Header></Header>
-  
-        <Routes>
-          <Route path="/" element={<PopularMovies />} />
-          <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
-          <Route path="/movie_details/:page/:id" element={<MovieDetails/>}/>
-          <Route path="/tv_shows_details/:page/:id" element={<TvShowDetails/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="main-container">
+      {/* if details then no header */}
+      {!isDetails&&<Header></Header>}
+
+      <Routes>
+        <Route path="/" element={<PopularMovies />} />
+        <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
+        <Route path="/movie_details/:page/:id" element={<MovieDetails />} />
+        <Route path="/tv_shows_details/:page/:id" element={<TvShowDetails />} />
+      </Routes>
+    </div>
   );
 }
 
