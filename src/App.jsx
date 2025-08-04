216be9c3e8ee27abcd84e18c6cc2ffa8c8735318
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-04 15:13:41 +0530

    Revert "moved BrowserRouter to App.jsx"
    This reverts commit 38df7acb5d4c0425cbabde6a27ae2b4aad3a429c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import PopularMovies from "./pages/movies/PopularMovies";
 import PopularTvShows from "./pages/tvShows/PopularTvShows";
@@ -7,22 +7,19 @@ import MovieDetails from "./pages/movies/MovieDetails";
 import TvShowDetails from "./pages/tvShows/TvShowDetails";
 
 function App() {
-  const location = useLocation();
-
-  const isDetailsPage =
-    location.pathname.startsWith("/movie_details") ||
-    location.pathname.startsWith("/tv_shows_details");
-
   return (
-    <div className="main-container">
-      {!isDetailsPage && <Header></Header>}
-      <Routes>
-        <Route path="/" element={<PopularMovies />} />
-        <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
-        <Route path="/movie_details/:page/:id" element={<MovieDetails />} />
-        <Route path="/tv_shows_details/:page/:id" element={<TvShowDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="main-container">
+        <Header></Header>
+  
+        <Routes>
+          <Route path="/" element={<PopularMovies />} />
+          <Route path="/popular_tv_shows/" element={<PopularTvShows />} />
+          <Route path="/movie_details/:page/:id" element={<MovieDetails/>}/>
+          <Route path="/tv_shows_details/:page/:id" element={<TvShowDetails/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
