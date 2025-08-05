5c2f42488d6e7b7e303d5d22a7011561f193e26d
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-05 11:22:53 +0530

    separated img and name+date using divs

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,40 @@
-import noImage from "/src/assets/noImage.png"
+import noImage from "/src/assets/noImage.png";
 import "./Card.css";
 import { Link } from "react-router-dom";
 
-const Card = ({ title, name, poster_path, release_date, first_air_date, id,page}) => {
-  
+const Card = ({
+  title,
+  name,
+  poster_path,
+  release_date,
+  first_air_date,
+  id,
+  page,
+}) => {
   return (
     <div className="card">
       {/* only movies have release date, so is this is true that means it movie else a tv show */}
-      <Link target="blank" to={`/${release_date?"movie_details":"tv_shows_details"}/${page}/${id}`}>
-      {/* remember _blank will open a new window every time */}
+      <Link
+        target="blank"
+        to={`/${
+          release_date ? "movie_details" : "tv_shows_details"
+        }/${page}/${id}`}
+      >
+        {/* remember _blank will open a new window every time */}
         <img
           className="poster"
-          src={poster_path?`https://image.tmdb.org/t/p/original${poster_path}`:noImage}
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/original${poster_path}`
+              : noImage
+          }
           alt=""
         />
       </Link>
-      <div className="title">{title || name}</div>
-      <div className="release-date">{release_date || first_air_date}</div>
+      <div> 
+        <div className="title">{title || name}</div>
+        <div className="release-date">{release_date || first_air_date}</div>
+      </div>
     </div>
   );
 };
