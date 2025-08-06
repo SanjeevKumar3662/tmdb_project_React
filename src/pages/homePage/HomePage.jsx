8b449e50e5652d45cabdf077c04d563c056fa6de
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-06 17:33:34 +0530

    added sliders, currently fetching from same endpoint

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,18 +1,29 @@
 import "./homePage.css";
-import PopularMovies from "../movies/PopularMovies";
+import SlidingCards from "../../components/slidingCards/SlidingCards";
 
 const homePage = () => {
   return (
     <main>
       <section className="hero">
         <div className="heroText">
-        <span>Welcome.</span>
-        <span>This is a Movie Database Website by SanjeevKumar3662</span>
+          <span>Welcome.</span>
+          <span>This is a Movie Database Website by SanjeevKumar3662</span>
         </div>
-        <input type="text" placeholder="Search for a movie or a person"/>
+        <input type="text" placeholder="Search for a movie or a person" />
       </section>
-      <section>
-        <PopularMovies></PopularMovies>
+
+      <section className="slide-container">
+        <h2>Popular Movies</h2>
+        <SlidingCards></SlidingCards>
+      </section>
+      <section className="slide-container">
+        <h2>Trending Movies</h2>
+        <SlidingCards></SlidingCards>
+      </section>
+      <section className="slide-container">
+        <h2>Popular Movies</h2>
+
+        <SlidingCards></SlidingCards>
       </section>
     </main>
   );
