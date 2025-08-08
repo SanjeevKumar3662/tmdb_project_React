4ee19405693468a7a85e5cb07d822a83f641d494
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-08 20:09:06 +0530

    added credit component base files

---
+++ src/components/mediaCredits/MediaCredits.jsx
@@ -0,0 +1,11 @@
+import "./mediaCredits.css";
+
+function MediaCredits() {
+  return (
+    <div className="credits-container">
+      <h1>MediaCredits</h1>
+    </div>
+  );
+}
+
+export default MediaCredits;
