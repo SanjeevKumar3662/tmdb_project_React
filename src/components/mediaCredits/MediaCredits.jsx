531ed13871bdb4c4c415621263ffea3ae6463f48
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-08 23:33:19 +0530

    added mediaCredits.jsx, fetches credit/cast info

--- src/components/mediaCredits/MediaCredits.jsx
+++ src/components/mediaCredits/MediaCredits.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from "react";
 import "./mediaCredits.css";
+import Card from "../card/Card";
+import SlidingCards from "../slidingCards/SlidingCards";
 
-function MediaCredits() {
+function MediaCredits({media_type,id}) {
+  const [media, setMedia] = useState(null);
+  // `https://first-backend-eight.vercel.app/media_credits/${media_type}/${id}`
+
+ useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://first-backend-eight.vercel.app/media_credits/${media_type}/${id}`
+        );
+        let data = await response.json();
+
+        setMedia(data);
+  console.log("media -> ",data.cast[0]);
+
+      } catch (e) {
+        console.log("error while fetching movie details", e);
+      }
+
+    };
+    fetchDetails();
+  }, [id, media_type]);
+
+  // console.log("media -> ",media);
+
   return (
-    <div className="credits-container">
-      <h1>MediaCredits</h1>
-    </div>
+    <>
+      {/* <SlidingCards media_type={"movie"} id={id}/> */}
+      <div className="credits-slider">
+        <h1>MediaCredits</h1>
+        {
+         media && <SlidingCards media_type={"credits"} credits={media.cast} />
+        }
+      </div>
+    </>
+      
+
   );
 }
 
