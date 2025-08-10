31023dc89994a8a758c061fdc77b558026ebe4b0
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-10 22:52:41 +0530

    added pageNav to top of the list page and removed background color for pageNav

--- src/pages/media/MediaLists.jsx
+++ src/pages/media/MediaLists.jsx
@@ -1,10 +1,10 @@
-import "./mediaLists.css"
+import "./mediaLists.css";
 import Card from "../../components/card/Card";
 import PageNav from "../../components/pageNav/PageNav";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-const MediaLists = ({media_type, list_type}) => {
+const MediaLists = ({ media_type, list_type, headerText }) => {
   const [movies, setMovies] = useState(null);
 
   //for getting page from url
@@ -25,7 +25,7 @@ const MediaLists = ({media_type, list_type}) => {
           `https://first-backend-eight.vercel.app/media_lists/${media_type}/${list_type}/${page}`
         );
         const data = await res.json();
-        console.log("pop-movie",data.results[0]);
+        console.log("pop-movie", data.results[0]);
 
         setMovies(data.results);
       };
@@ -36,15 +36,28 @@ const MediaLists = ({media_type, list_type}) => {
 
     //this will scroll to top
     window.scrollTo({ top: 0 });
-  }, [page,media_type,list_type]);
+  }, [page, media_type, list_type]);
 
   // console.log(movies[0]);
   return (
     <div className="movie-container">
-      <h1>Popular Movies</h1>
+      <h1>{headerText}</h1>
+      <PageNav
+        prevClick={() => setPage(page > 1 ? () => page - 1 : page)}
+        nextClick={() => setPage(() => page + 1)}
+        page={page}
+        setPage={setPage}
+      ></PageNav>
       <div className="flex-container">
         {movies ? (
-          movies.map((movie) => <Card key={movie.id} page={page} cssClass={"card"} {...movie}></Card>)
+          movies.map((movie) => (
+            <Card
+              key={movie.id}
+              page={page}
+              cssClass={"card"}
+              {...movie}
+            ></Card>
+          ))
         ) : (
           <h1>Loading...</h1>
         )}
