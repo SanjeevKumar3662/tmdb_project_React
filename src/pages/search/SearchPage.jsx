eaee30a93d5e941b2901602d970413b687707f4c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-14 00:16:35 +0530

    added base search fuction

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,12 +1,38 @@
-import "./searchPage.css"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./searchPage.css";
+import Card from "../../components/card/Card";
 
-const SearchPage = () => {
+const SearchPage = ({ query_type = "movie", page = 1 }) => {
+  const [searchRes, setSearchRes] = useState(null);
+  const { query } = useParams();
+  useEffect(() => {
+    try {
+      const fetchSearchResults = async () => {
+        const res = await fetch(
+          `https://first-backend-eight.vercel.app/search/${query_type}/${query}/${page}`
+        );
+        const data = await res.json();
+        console.log(data.results);
+        setSearchRes(data.results);
+      };
+      fetchSearchResults();
+    } catch (e) {
+      console.error(e);
+    }
+  }, [query_type, query, page]);
+
   return (
-    <div>
+    <>
+      <h1>Base Search Fuction : More Updates Comming..</h1>
+      <div className="search-container">
+        {searchRes &&
+          searchRes.map((ele) => {
+            return <Card key={ele.id} cssClass={"card"} {...ele} />;
+          })}
+      </div>
+    </>
+  );
+};
 
-    <h1>search fuction</h1>
-    </div>
-  )
-}
-
-export default SearchPage
+export default SearchPage;
