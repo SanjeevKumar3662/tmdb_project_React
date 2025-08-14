ffcde1ef47a7e8502a1ed0b2b99c7b2c1e2cda6b
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-14 19:44:46 +0530

    added pageNav component and it's styles

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,10 +1,29 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import "./searchPage.css";
 import Card from "../../components/card/Card";
+import PageNav from "../../components/pageNav/PageNav";
 
-const SearchPage = ({ query_type = "movie", page = 1 }) => {
+const SearchPage = ({ query_type = "movie"}) => {
   const [searchRes, setSearchRes] = useState(null);
+
+   //for getting page from url
+  const [searchParams, setSearchParams] = useSearchParams(); //used to read query params from url
+  //this returns a string so parse it
+  const pageFromURL = parseInt(searchParams.get("page") || 1);
+  const [page, setPage] = useState(pageFromURL);
+
+  useEffect(() => {
+    setSearchParams({ page }); //updates the page sting in url
+  }, [page, setSearchParams]);
+  //end
+
+  //this will reset the page to 1, when we choose a dif list
+  useEffect(() => {
+    setPage(1);
+  }, []);
+
+
   const { query } = useParams();
   useEffect(() => {
     try {
@@ -29,12 +48,24 @@ const SearchPage = ({ query_type = "movie", page = 1 }) => {
         <span>Total pages {searchRes && searchRes.total_pages}</span>
         <span>Total Results : {searchRes && searchRes.total_results}</span>
       </div>
+      <PageNav
+        prevClick={() => setPage(page > 1 ? () => page - 1 : page)}
+        nextClick={() => setPage(() => page + 1)}
+        page={page}
+        setPage={setPage}
+      ></PageNav>
       <div className="flex-search-container">
         {searchRes &&
           searchRes.results.map((ele) => {
             return <Card key={ele.id} cssClass={"card"} {...ele} />;
           })}
       </div>
+       <PageNav
+        prevClick={() => setPage(page > 1 ? () => page - 1 : page)}
+        nextClick={() => setPage(() => page + 1)}
+        page={page}
+        setPage={setPage}
+      ></PageNav>
     </div>
   );
 };
