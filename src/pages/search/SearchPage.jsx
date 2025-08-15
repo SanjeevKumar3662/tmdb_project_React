ae6349b7abef033790647e36d27c27183ecccdf2
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-15 10:19:53 +0530

    added base query_type slider

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -3,11 +3,22 @@ import { useParams, useSearchParams } from "react-router-dom";
 import "./searchPage.css";
 import Card from "../../components/card/Card";
 import PageNav from "../../components/pageNav/PageNav";
+import Slider from "react-slick";
 
-const SearchPage = ({ query_type = "movie"}) => {
+const SearchPage = () => {
   const [searchRes, setSearchRes] = useState(null);
+  const [query_type, setQuery_type] = useState("movie");
 
-   //for getting page from url
+  const queryArr = [
+    "movie",
+    "tv",
+    "person",
+    "collection",
+    "company",
+    "keyword",
+  ];
+
+  //for getting page from url
   const [searchParams, setSearchParams] = useSearchParams(); //used to read query params from url
   //this returns a string so parse it
   const pageFromURL = parseInt(searchParams.get("page") || 1);
@@ -22,7 +33,6 @@ const SearchPage = ({ query_type = "movie"}) => {
   useEffect(() => {
     setPage(1);
   }, []);
-
 
   const { query } = useParams();
   useEffect(() => {
@@ -41,9 +51,62 @@ const SearchPage = ({ query_type = "movie"}) => {
     }
   }, [query_type, query, page]);
 
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 200,
+    slidesToShow: 8,
+    slidesToScroll: 5,
+    responsive: [
+      {
+        breakpoint: 1350, // tablets
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1024, // tablets
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+      {
+        breakpoint: 768, // small tablets / large phones
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+      {
+        breakpoint: 550, // tablets
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 480, // mobile phones
+        settings: {
+          slidesToShow: 2.05,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="search-container">
       {/* <h1>Base Search Fuction ...</h1> */}
+      <div className="query-slider">
+        <Slider {...settings}>
+          {queryArr &&
+            queryArr.map((type) => (
+              <button onClick={() => setQuery_type(type)}>{type}</button>
+            ))}
+        </Slider>
+      </div>
       <div>
         <span>Total pages {searchRes && searchRes.total_pages}</span>
         <span>Total Results : {searchRes && searchRes.total_results}</span>
@@ -60,7 +123,7 @@ const SearchPage = ({ query_type = "movie"}) => {
             return <Card key={ele.id} cssClass={"card"} {...ele} />;
           })}
       </div>
-       <PageNav
+      <PageNav
         prevClick={() => setPage(page > 1 ? () => page - 1 : page)}
         nextClick={() => setPage(() => page + 1)}
         page={page}
