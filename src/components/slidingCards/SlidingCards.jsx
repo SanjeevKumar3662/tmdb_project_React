bf7e89b3c70e4f5b3cbe3c97377fd64d927aeb74
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-16 23:48:42 +0530

    using  SlidingVideos component, now sliding cards will also render video cards

--- src/components/slidingCards/SlidingCards.jsx
+++ src/components/slidingCards/SlidingCards.jsx
@@ -2,6 +2,7 @@ import "./slidingCards.css";
 import Card from "../../components/card/Card";
 import { useEffect, useState } from "react";
 // import { useSearchParams } from "react-router-dom";
+import SlidingVideos from "../../components/slidingVideos/SlidingVideos";
 
 //for slider
 import Slider from "react-slick";
@@ -9,7 +10,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 //slider end
 
-const SlidingCards = ({ media_type, list_type, credits }) => {
+const SlidingCards = ({ media_type, list_type, credits, videos }) => {
   const [movies, setMovies] = useState(null);
   // const [infiniteScroll, setInfiniteScroll] = useState(true);
   // infiniteScroll && credits.length <= 8 && setInfiniteScroll(false);
@@ -35,47 +36,46 @@ const SlidingCards = ({ media_type, list_type, credits }) => {
 
   // console.log(movies[0]);
 
-
   const settings = {
     dots: false,
     infinite: credits && credits.length <= 8 ? false : true,
     speed: 200,
-    slidesToShow: 8,
-    slidesToScroll: 5,
+    slidesToShow: videos ? 2 : 8,
+    slidesToScroll: videos ? 1 : 5,
     responsive: [
       {
         breakpoint: 1350, // tablets
         settings: {
-          slidesToShow: 6,
-          slidesToScroll: 3,
+          slidesToShow: videos ? 2 : 6,
+          slidesToScroll: videos ? 2 : 3,
         },
       },
       {
         breakpoint: 1024, // tablets
         settings: {
-          slidesToShow: 5,
-          slidesToScroll: 3,
+          slidesToShow: videos ? 2 : 5,
+          slidesToScroll: videos ? 2 : 3,
         },
       },
       {
         breakpoint: 768, // small tablets / large phones
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 3,
+          slidesToShow: videos ?1 : 4,
+          slidesToScroll: videos ? 1 : 3,
         },
       },
       {
         breakpoint: 550, // tablets
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToShow: videos ? 1 : 3,
+          slidesToScroll: videos ? 1 : 2,
         },
       },
       {
         breakpoint: 420, // mobile phones
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: videos ? 1 : 2,
+          slidesToScroll: videos ? 1 : 2,
         },
       },
     ],
@@ -99,6 +99,11 @@ const SlidingCards = ({ media_type, list_type, credits }) => {
             <div key={person.id}>
               <Card cssClass={"sliding-cards"} {...person}></Card>
             </div>
+          ))}
+
+        {videos &&
+          videos.results.map((video) => (
+            <SlidingVideos title={video.name} key={video.id} videoId={video.key} /> 
           ))}
       </Slider>
     </>
