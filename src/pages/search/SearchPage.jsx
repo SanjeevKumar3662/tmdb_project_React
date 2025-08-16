e4ebaee75a65e6781a38ed545627393fb6fde3c1
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-16 19:38:18 +0530

    now when query_type is changed, page will become 1

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -8,37 +8,25 @@ import SearchInput from "../../components/searchInput/SearchInput";
 
 const SearchPage = () => {
   const [searchRes, setSearchRes] = useState(null);
-  const [query_type, setQuery_type] = useState("movie");
-
-  const queryArr = [
-    {
-      query: "multi",
-      text: "All",
-      info: "Shows Combination of Movie, TV and People",
-    },
-    { query: "movie", text: "Movie", info: "Shows movie results" },
-    { query: "tv", text: "TV", info: "Shows tv results" },
-    { query: "person", text: "Person", info: "Shows persons" },
-    { query: "collection", text: "Collection", info: "Shows Collections" },
-    { query: "company", text: "Company", info: "Shows Companies" },
-    { query: "keyword", text: "Keywords", info: "Lists related keywords" },
-  ];
-
   //for getting page from url
   const [searchParams, setSearchParams] = useSearchParams(); //used to read query params from url
+  const queryTypeFromURL = searchParams.get("query_type") || "movie";
+
+  const [query_type, setQuery_type] = useState(queryTypeFromURL);
+
   //this returns a string so parse it
   const pageFromURL = parseInt(searchParams.get("page") || 1);
   const [page, setPage] = useState(pageFromURL);
 
   useEffect(() => {
-    setSearchParams({ page }); //updates the page sting in url
-  }, [page, setSearchParams]);
+    setSearchParams({ query_type, page }); //updates the page sting in url
+  }, [query_type, page, setSearchParams]);
   //end
 
   //this will reset the page to 1, when we choose a dif list
   useEffect(() => {
     setPage(1);
-  }, []);
+  }, [query_type]);
 
   const { query } = useParams();
   useEffect(() => {
@@ -102,10 +90,24 @@ const SearchPage = () => {
     ],
   };
 
+  const queryArr = [
+    {
+      query: "multi",
+      text: "All",
+      info: "Shows Combination of Movie, TV and People",
+    },
+    { query: "movie", text: "Movie", info: "Shows movie results" },
+    { query: "tv", text: "TV", info: "Shows tv results" },
+    { query: "person", text: "Person", info: "Shows persons" },
+    { query: "collection", text: "Collection", info: "Shows Collections" },
+    { query: "company", text: "Company", info: "Shows Companies" },
+    { query: "keyword", text: "Keywords", info: "Lists related keywords" },
+  ];
+
   return (
     <div className="search-container">
       {/* <h1>Base Search Fuction ...</h1> */}
-      <SearchInput/>
+      <SearchInput />
       <div className="query-slider">
         <Slider {...settings}>
           {queryArr &&
@@ -132,7 +134,7 @@ const SearchPage = () => {
       <div className="flex-search-container">
         {searchRes &&
           searchRes.results.map((ele) => {
-            return <Card key={ele.id} cssClass={"card"} {...ele} />;
+            return <Card page={page} key={ele.id} cssClass={"card"} {...ele} />;
           })}
       </div>
       <PageNav
