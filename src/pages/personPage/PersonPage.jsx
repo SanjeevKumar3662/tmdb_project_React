430c63176b0f4063b72906d248177566679af3dd
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-23 20:46:08 +0530

    added person page jsx and css files

---
+++ src/pages/personPage/PersonPage.jsx
@@ -0,0 +1,11 @@
+import "./personPage.css";
+
+const PersonPage = () => {
+  return (
+    <div className="person-page-container">
+      <h1>this is a page for persons</h1>
+    </div>
+  );
+};
+
+export default PersonPage;
