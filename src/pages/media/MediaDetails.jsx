0c0011cb2bf81b3aa67310d25b423e81586e00f7
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-08-29 18:18:30 +0530

    show a loader spinner while is loading is true

--- src/pages/media/MediaDetails.jsx
+++ src/pages/media/MediaDetails.jsx
@@ -28,7 +28,16 @@ const MovieDetails = ({ media_type }) => {
 
   if (isLoading) {
     return (
-      <div style={{ height: "100vh", backgroundColor: "white" ,fontSize:"2rem"}}>Loading</div>
+      <div
+        style={{
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <span className="loader"></span>
+      </div>
     );
   }
 
@@ -36,89 +45,91 @@ const MovieDetails = ({ media_type }) => {
     return <div>Error in media details page</div>;
   }
 
-  isSuccess && window.scrollTo(0,0)//scrolls to top
+  isSuccess && window.scrollTo(0, 0); //scrolls to top
 
   return (
-   isSuccess && <>
-      <div className="details-container">
-        <img
-          className="details-bg"
-          src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
-          loading="eager"
-          fetchPriority="high"
-          decoding="async"
-        />
+    (
+      <>
+        <div className="details-container">
+          <img
+            className="details-bg"
+            src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+            loading="eager"
+            fetchPriority="high"
+            decoding="async"
+          />
 
-        <section className="poster-details">
-          {movie.poster_path ? (
-            <img
-              className="poster"
-              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-              srcSet={`https://image.tmdb.org/t/p/w342${movie.poster_path} 1x,
+          <section className="poster-details">
+            {movie.poster_path ? (
+              <img
+                className="poster"
+                src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                srcSet={`https://image.tmdb.org/t/p/w342${movie.poster_path} 1x,
             https://image.tmdb.org/t/p/w500${movie.poster_path} 2x`}
-              alt="poster"
-              loading="eager"
-              decoding="async"
-            />
-          ) : (
-            <img src="/noImage.png" loading="lazy" decoding="async" />
-          )}
-        </section>
-        <section className="more-info">
-          <span className="media-page-heading">
-            {movie.title ? movie.title : movie.name}
-          </span>
-          {(movie.original_title || movie.original_name) && (
+                alt="poster"
+                loading="eager"
+                decoding="async"
+              />
+            ) : (
+              <img src="/noImage.png" loading="lazy" decoding="async" />
+            )}
+          </section>
+          <section className="more-info">
+            <span className="media-page-heading">
+              {movie.title ? movie.title : movie.name}
+            </span>
+            {(movie.original_title || movie.original_name) && (
+              <div>
+                Original Title : {movie.original_title || movie.original_name}
+              </div>
+            )}
+            {movie.tagline && <div>Tagline : {movie.tagline}</div>}
+            <div className="title">
+              Discription :<span> {movie.overview}</span>
+            </div>
+            {movie.release_date ? (
+              <div className="release-date">
+                Release Date : {movie.release_date}
+              </div>
+            ) : (
+              <div className="release-date"> {movie.first_air_date}</div>
+            )}
             <div>
-              Original Title : {movie.original_title || movie.original_name}
+              Genres :{" "}
+              {movie.genres &&
+                movie.genres.map((ele) => (
+                  <span key={ele.id}>{ele.name} , </span>
+                ))}
             </div>
-          )}
-          {movie.tagline && <div>Tagline : {movie.tagline}</div>}
-          <div className="title">
-            Discription :<span> {movie.overview}</span>
-          </div>
-          {movie.release_date ? (
-            <div className="release-date">
-              Release Date : {movie.release_date}
-            </div>
-          ) : (
-            <div className="release-date"> {movie.first_air_date}</div>
-          )}
-          <div>
-            Genres :{" "}
-            {movie.genres &&
-              movie.genres.map((ele) => (
-                <span key={ele.id}>{ele.name} , </span>
-              ))}
-          </div>
+          </section>
+        </div>
+        <div className="video-container">
+          <MediaContentSlider
+            media_type={media_type}
+            content_type={"videos"}
+            id={id}
+          />
+        </div>
+        <div className="backdrops-container">
+          <MediaContentSlider
+            media_type={media_type}
+            content_type={"images"}
+            id={id}
+          />
+        </div>
+        <section className="credits-container">
+          <MediaCredits media_type={media_type} id={id} />
         </section>
-      </div>
-      <div className="video-container">
-        <MediaContentSlider
-          media_type={media_type}
-          content_type={"videos"}
-          id={id}
-        />
-      </div>
-      <div className="backdrops-container">
-        <MediaContentSlider
-          media_type={media_type}
-          content_type={"images"}
-          id={id}
-        />
-      </div>
-      <section className="credits-container">
-        <MediaCredits media_type={media_type} id={id} />
-      </section>
-      <div className="rec-slider">
-        <p className="section-heading">Recommendations</p>
-        <MediaContentSlider
-          media_type={media_type}
-          content_type={"recommendations"}
-          id={id}
-        />
-      </div>
-    </>
+        <div className="rec-slider">
+          <p className="section-heading">Recommendations</p>
+          <MediaContentSlider
+            media_type={media_type}
+            content_type={"recommendations"}
+            id={id}
+          />
+        </div>
+      </>
+    )
   );
 };
 
