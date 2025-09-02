fcbc0dabfb07315ec036f4e374da850e3a55b908
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-02 22:46:34 +0530

    added a slider for PersonPage movie and tv credits

---
+++ src/pages/personPage/PersonCreditSlider.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Card from "../../components/card/Card";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export const PersonCreditSlider = ({ personCredits, link }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 8,
+    slidesToScroll: 5,
+    lazyLoadBuffer: 3,
+    responsive: [
+      {
+        breakpoint: 1350, // tablets
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1024, // tablets
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768, // small tablets / large phones
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 620, // tablets
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 520, // mobile phones
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
+  //  personCredits && console.log(personCredits);
+  return (
+    <Slider {...settings}>
+      {personCredits &&
+        personCredits.cast.map((person) => (
+          <div key={person.id}>
+            <Card cssClass={"sliding-cards"} {...person} linkTo={link}></Card>
+          </div>
+        ))}
+    </Slider>
+  );
+};
