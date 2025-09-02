36af9e09733c354e39555b8a90ee97fc0ce7a35c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-02 22:29:15 +0530

    Replaced Sliding Cards in mediaCredits with slider and cards, now MediaCredits does end to end work for cast cards

--- src/components/mediaCredits/MediaCredits.jsx
+++ src/components/mediaCredits/MediaCredits.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import "./mediaCredits.css";
-import SlidingCards from "../slidingCards/SlidingCards";
+import Card from "../card/Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MediaCredits({ media_type, id }) {
   const [media, setMedia] = useState(null);
-  // `https://first-backend-eight.vercel.app/media_credits/${media_type}/${id}`
+  
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -24,13 +27,69 @@ function MediaCredits({ media_type, id }) {
   }, [id, media_type]);
 
   // console.log("media -> ",media);
+const settings = {
+    dots: false,
+    infinite: media && media.cast.length <= 8 ? false : true,
+    speed: 200,
+    slidesToShow: 8,
+    slidesToScroll: 5,
+    lazyLoadBuffer: 3,
+    responsive: [
+      {
+        breakpoint: 1350, // tablets
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 1024, // tablets
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768, // small tablets / large phones
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 620, // tablets
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 520, // mobile phones
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
   return (
     <>
       {/* <SlidingCards media_type={"movie"} id={id}/> */}
       <div className="credits-slider">
         <h1>Credits</h1>
-        {media && <SlidingCards media_type={"credits"} credits={media.cast} />}
+        <Slider {...settings}>
+          {media &&
+            media.cast.map((person) => (
+              <div key={person.id}>
+                <Card
+                  cssClass={"sliding-cards"}
+                  {...person}
+                  linkTo={"person_details"}
+                ></Card>
+              </div>
+            ))}
+        </Slider>
       </div>
     </>
   );
