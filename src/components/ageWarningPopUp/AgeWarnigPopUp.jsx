405d937d595d0ab60051f474e64cf6617d780ae3
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-04 23:05:23 +0530

    added ageWarningPopUp component and it's related css

---
+++ src/components/ageWarningPopUp/AgeWarnigPopUp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./ageWarningPopUp.css"; // we'll create this file
+
+export default function AgeWarningPopup({ setConcent }) {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleConfirm = () => {
+    setShowPopup(false);
+    setConcent(true);
+  };
+  const handleExit = () => {
+    setShowPopup(false);
+    setConcent(false);
+  };
+
+  if (!showPopup) return null;
+
+  return (
+    <div className="popup-overlay">
+      <div className="popup-box">
+        <h2>Age Restricted Content</h2>
+        <p>
+          This movie/TV show is marked as 18+. Are you sure you want to
+          continue?
+        </p>
+        <div className="popup-buttons">
+          <button className="confirm-btn" onClick={handleConfirm}>
+            Yes, Continue
+          </button>
+          <button className="exit-btn" onClick={handleExit}>
+            Exit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
