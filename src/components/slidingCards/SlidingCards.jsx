315213da918784e7954839af41b5cc9675061bad
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-09-07 16:27:08 +0530

    added 200px height to isPending loader in Sliding cards

--- src/components/slidingCards/SlidingCards.jsx
+++ src/components/slidingCards/SlidingCards.jsx
@@ -46,7 +46,7 @@ const SlidingCards = ({
     return (
       <div
         style={{
-          height: "100%",
+          height: "200px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
